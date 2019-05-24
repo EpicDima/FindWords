f733758510401aa1f2fa3d688cab879477cc7a26
Dima <dima2-8@bk.ru>
2019-05-25 01:12:20 +0300

    FindWords Версия 2. Без классов

--- FindWords/FindWords.cpp
+++ FindWords/FindWords.cpp
@@ -5,24 +5,9 @@
 
 using namespace std;
 
-/*
-vector<string> f(vector<string> words, string chars, unsigned int index)
-{
-	vector<string> newWords;
-	unsigned int size = words.size();
-	unsigned int charSize = chars.size();
-	for (unsigned int i = 0; i < size; i++) {
-		if (words[i].size() < index + 1) {
-			newWords.push_back(words[i]);
-		} else {
-			for (unsigned int j = 0; j < charSize; j++) {
-				if (words[i][index] == chars[j])
-			words[i][index] == ch || w
-			}
-		}
-	}
-	return newWords;
-}*/
+
+int dx[] = { 1, -1, 0, 0 };
+int dy[] = { 0, 0, 1, -1 };
 
 unsigned int n;
 unsigned int m;
@@ -30,78 +15,6 @@ string* matrix;
 
 const unsigned int minLength = 4;
 const unsigned int maxLength = 10;
-
-class Node
-{
-public:
-
-	Node(string word, int x, int y, bool** mask)
-	{
-		this->word = word;
-		this->x = x;
-		this->y = y;
-		this->mask = mask;
-
-		pressF();
-	}
-
-	vector<string> getWords()
-	{
-		vector<string> a;
-		if (this->word.size() >= minLength) {
-			a.push_back(this->word);
-		}
-		
-		for (int i = 0; i < this->nodes.size(); i++) {
-			vector<string> t = this->nodes[i]->getWords();
-			for (int j = 0; j < t.size(); j++) {
-				a.push_back(t[j]);
-			}
-		}
-		return a;
-	}
-
-	static int dx[4];
-	static int dy[4];
-
-
-private:
-
-	void pressF()
-	{
-		if (this->word.size() >= maxLength) {
-			return;
-		}
-		int x2;
-		int y2;
-		for (int i = 0; i < 4; i++) {
-			x2 = this->x + this->dx[i];
-			y2 = this->y + this->dy[i];
-			if (x2 >= 0 && x2 < n && y2 >= 0 && y2 < m) {
-				if (!this->mask[x2][y2]) {
-					bool** mask2 = new bool*[n];
-					for (int j = 0; j < n; j++) {
-						mask2[j] = new bool[m];
-						for (int h = 0; h < m; h++) {
-							mask2[j][h] = this->mask[j][h];
-						}
-					}
-					mask2[x2][y2] = true;
-					string word2 = this->word + matrix[x2][y2];
-					this->nodes.push_back(new Node(word2, x2, y2, mask2));
-				}
-			}
-		}
-	}
-
-	int x, y;
-	bool** mask;
-	string word;
-	vector<Node*> nodes;
-};
-
-int Node::dx[] = { 1, -1, 0, 0 };
-int Node::dy[] = { 0, 0, 1, -1 };
 
 
 void input()
@@ -119,6 +32,46 @@ void input()
 	}
 }
 
+
+vector<string> f(string word, int x, int y, bool** mask)
+{
+	vector<string> newWords;
+	if (word.size() >= minLength) {
+		newWords.push_back(word);
+	}
+	
+	if (word.size() >= maxLength) {
+		return newWords;
+	}
+	int x2;
+	int y2;
+	for (int i = 0; i < 4; i++) {
+		x2 = x + dx[i];
+		y2 = y + dy[i];
+		if (x2 >= 0 && x2 < n && y2 >= 0 && y2 < m) {
+			if (!mask[x2][y2]) {
+				bool** mask2 = new bool*[n];
+				for (int j = 0; j < n; j++) {
+					mask2[j] = new bool[m];
+					for (int h = 0; h < m; h++) {
+						mask2[j][h] = mask[j][h];
+					}
+				}
+				mask2[x2][y2] = true;
+				string word2 = word + matrix[x2][y2];
+				
+				vector<string> temp = f(word2, x2, y2, mask2);
+				for (int i = 0; i < temp.size(); i++) {
+					newWords.push_back(temp[i]);
+				}
+			}
+		}
+	}
+
+	return newWords;
+}
+
+
 vector<string> findAllPossibleWords()
 {
 	vector<string> a;
@@ -134,8 +87,7 @@ vector<string> findAllPossibleWords()
 	for (unsigned int i = 0; i < n; i++) {
 		for (unsigned int j = 0; j < m; j++) {
 			mask[i][j] = true;
-			Node tempNode(string(1, matrix[i][j]), i, j, mask);
-			vector<string> q = tempNode.getWords();
+			vector<string> q = f(string(1, matrix[i][j]), i, j, mask);
 			for (int h = 0; h < q.size(); h++) {
 				a.push_back(q[h]);
 			}
@@ -190,14 +142,13 @@ int main()
 	}
 	
 	
-
-	//for (unsigned int i = 0; i < possibleWords.size(); i++) {
-	//	cout << possibleWords[i] << endl;
-	//}
-
-	for (unsigned int i = 0; i < matchedWords.size(); i++) {
-		cout << matchedWords[i] << endl;
+	for (unsigned int i = 0; i < possibleWords.size(); i++) {
+		cout << possibleWords[i] << endl;
 	}
+
+	//for (unsigned int i = 0; i < matchedWords.size(); i++) {
+	//	cout << matchedWords[i] << endl;
+	//}
 
 	return 0;
 }
